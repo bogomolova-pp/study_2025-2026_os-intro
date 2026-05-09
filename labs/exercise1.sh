9e1697197e1bae44994aad344a4a5bb8632a514c
Polina Bogomolova <1032253562@rudn.ru>
2026-05-09 09:46:52 +0300

    rlese

--- labs/exercise1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BACKUP_DIR="$HOME/backup"
-SCRIPT_PATH=$(realpath "$0")
-SCRIPT_NAME=$(basename "$0")
-TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-mkdir -p "$BACKUP_DIR"
-tar -czf "$BACKUP_DIR/${SCRIPT_NAME}_${TIMESTAMP}.tar.gz" "$SCRIPT_PATH"
